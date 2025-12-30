e03dceb59583054207c1123cba8515bdb7fd33f5
eun-seoo <kimeunseo040@gmail.com>
2025-12-30 22:37:19 +0900

    Initial Commit

---
+++ src/main/kotlin/com/stepbookstep/StepbookstepApplication.kt
@@ -0,0 +1,11 @@
+package com.stepbookstep
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StepbookstepApplication
+
+fun main(args: Array<String>) {
+    runApplication<StepbookstepApplication>(*args)
+}
