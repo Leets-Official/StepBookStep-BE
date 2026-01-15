e594ab8b788b0c72eaa8822b96cebb2a8777e91d
eun-seoo <kimeunseo040@gmail.com>
2026-01-15 23:04:07 +0900

    feat: 레벨별 랜덤 도서 조회 및 키워드 검색 쿼리 추가

---
+++ src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookRepository.kt
@@ -0,0 +1,24 @@
+package com.stepbookstep.server.domain.book.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
+
+@Repository
+interface BookRepository : JpaRepository<Book, Long> {
+
+    fun findByIsbn13(isbn13: String): Book?
+
+    @Query(
+        value = "SELECT * FROM books_new WHERE level = :level ORDER BY RAND() LIMIT 4",
+        nativeQuery = true
+    )
+    fun findRandomByLevel(@Param("level") level: Int): List<Book>
+
+    @Query("""
+        SELECT b FROM Book b
+        WHERE b.title LIKE %:keyword% OR b.author LIKE %:keyword% OR b.publisher LIKE %:keyword%
+    """)
+    fun searchByKeyword(@Param("keyword") keyword: String): List<Book>
+}
