98cc69cb5867d217af45398fc9091d42fc5ffc09
eun-seoo <kimeunseo040@gmail.com>
2026-01-15 23:02:14 +0900

    feat: 장르 ENUM 추가

---
+++ src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookGenre.kt
@@ -0,0 +1,10 @@
+package com.stepbookstep.server.domain.book.domain
+
+enum class BookGenre(val displayName: String) {
+    KOREAN_NOVEL_2000("2000년대 이후 한국소설"),
+    JAPANESE_NOVEL_1950("1950년대 이후 일본소설"),
+    CHINESE_NOVEL("중국소설"),
+    ENGLISH_NOVEL("영미소설"),
+    FRENCH_NOVEL("프랑스소설"),
+    GERMAN_NOVEL("독일소설")
+}
