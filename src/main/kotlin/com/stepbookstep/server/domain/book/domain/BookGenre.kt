e80733e9609d8156a85bd49a349376c04dc9c345
eun-seoo <kimeunseo040@gmail.com>
2026-01-16 09:57:01 +0900

    fix: 도서 장르 ENUM의 displayName 수정

--- src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookGenre.kt
+++ src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookGenre.kt
@@ -1,8 +1,8 @@
 package com.stepbookstep.server.domain.book.domain
 
 enum class BookGenre(val displayName: String) {
-    KOREAN_NOVEL_2000("2000년대 이후 한국소설"),
-    JAPANESE_NOVEL_1950("1950년대 이후 일본소설"),
+    KOREAN_NOVEL("한국소설"),
+    JAPANESE_NOVEL("일본소설"),
     CHINESE_NOVEL("중국소설"),
     ENGLISH_NOVEL("영미소설"),
     FRENCH_NOVEL("프랑스소설"),
