3d5bba8dab3afae8cc015fc669fbcdf53d8f9632
eun-seoo <kimeunseo040@gmail.com>
2026-01-16 09:56:01 +0900

    refactor: 불필요한 어노테이션 삭제

--- src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookRepository.kt
+++ src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookRepository.kt
@@ -3,9 +3,7 @@ package com.stepbookstep.server.domain.book.domain
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.stereotype.Repository
 
-@Repository
 interface BookRepository : JpaRepository<Book, Long> {
 
     fun findByIsbn13(isbn13: String): Book?
