ce5f8c060cf2117cb55bf371e2c43cd0b064c651
eun-seoo <kimeunseo040@gmail.com>
2026-01-19 22:31:39 +0900

    feat: 홈 목록 조회 캐시 적용

---
+++ src/main/kotlin/com/stepbookstep/server/domain/home/application/HomeCacheService.kt
@@ -0,0 +1,29 @@
+package com.stepbookstep.server.domain.home.application
+
+import com.stepbookstep.server.domain.book.domain.Book
+import com.stepbookstep.server.domain.book.domain.BookRepository
+import org.springframework.cache.annotation.Cacheable
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class HomeCacheService(
+    private val bookRepository: BookRepository
+) {
+
+    @Cacheable(value = ["genreBooks"], key = "#genre")
+    fun getGenreBooks(genre: String): List<Book> {
+        return bookRepository.findRandomByGenre(genre)
+    }
+
+    @Cacheable(value = ["under200Books"])
+    fun getUnder200Books(): List<Book> {
+        return bookRepository.findUnder200Pages()
+    }
+
+    @Cacheable(value = ["bestsellerBooks"])
+    fun getBestsellerBooks(): List<Book> {
+        return bookRepository.findBestsellers()
+    }
+}
