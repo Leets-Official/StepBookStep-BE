3ffb57cf0938e104f0584e14ac7c4600ba84e5c9
장유정 <jyj030591@gmail.com>
2026-01-20 14:43:35 +0900

    fix: 카카오 로그인 API 인증 인터셉터 제외

--- src/main/kotlin/com/stepbookstep/server/security/config/WebConfig.kt
+++ src/main/kotlin/com/stepbookstep/server/security/config/WebConfig.kt
@@ -16,7 +16,7 @@ class WebConfig(
 
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(authenticationInterceptor)
-            // 헬스체크, 스웨거 제외 userId 여부로 판단됩니다.
-            .excludePathPatterns("/health", "/swagger-ui/**", "/v3/api-docs/**")
+            .addPathPatterns("/api/**")
+            .excludePathPatterns("/health", "/swagger-ui/**", "/v3/api-docs/**", "/api/v1/auth/**")
     }
 }
