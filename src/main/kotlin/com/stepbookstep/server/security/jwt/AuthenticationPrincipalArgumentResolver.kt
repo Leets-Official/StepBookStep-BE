7ae24c93599c8dea11d5a8bfb96b3e7340bc48a0
eun-seoo <kimeunseo040@gmail.com>
2026-01-23 15:28:31 +0900

    feat: @LoginUserId 처리를 위한 ArgumentResolver 구현

---
+++ src/main/kotlin/com/stepbookstep/server/security/jwt/AuthenticationPrincipalArgumentResolver.kt
@@ -0,0 +1,30 @@
+package com.stepbookstep.server.security.jwt
+
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.context.request.RequestAttributes
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+/**
+ * @LoginUserId 어노테이션이 붙은 파라미터에
+ * request attribute의 userId를 주입하는 ArgumentResolver
+ */
+@Component
+class AuthenticationPrincipalArgumentResolver : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(LoginUserId::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Long? {
+        return webRequest.getAttribute("userId", RequestAttributes.SCOPE_REQUEST) as? Long
+    }
+}
