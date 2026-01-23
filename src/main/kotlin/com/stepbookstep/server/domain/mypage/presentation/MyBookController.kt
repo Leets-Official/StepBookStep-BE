b6eb62b219d048ce194d091fea1974dad4aa20b6
eun-seoo <kimeunseo040@gmail.com>
2026-01-23 15:31:56 +0900

    refactor: 유저 ID 주입 방식을 @LoginUserId로 변경

--- src/main/kotlin/com/stepbookstep/server/domain/mypage/presentation/MyBookController.kt
+++ src/main/kotlin/com/stepbookstep/server/domain/mypage/presentation/MyBookController.kt
@@ -3,6 +3,7 @@ package com.stepbookstep.server.domain.mypage.presentation
 import com.stepbookstep.server.domain.mypage.application.MyBookQueryService
 import com.stepbookstep.server.domain.mypage.application.dto.MyBooksResponse
 import com.stepbookstep.server.domain.mypage.domain.MyShelf
+import com.stepbookstep.server.security.jwt.LoginUserId
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.media.Schema
@@ -10,7 +11,6 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
-import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @Tag(name = "MyPage", description = "마이페이지(내 서재) API")
@@ -47,7 +47,7 @@ class MyBookController(
         pageable: Pageable,
 
         @Parameter(hidden = true)
-    @AuthenticationPrincipal userId: Long
+        @LoginUserId userId: Long
     ): MyBooksResponse {
         val tab = MyShelf.from(readStatus)
         val result = myBookQueryService.getMyBooks(userId, tab, pageable)
