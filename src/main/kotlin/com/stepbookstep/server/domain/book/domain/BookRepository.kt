82b4889f1ccb0be536b32548668305ad3f54631d
eun-seoo <kimeunseo040@gmail.com>
2026-01-25 01:24:21 +0900

    refactor: 도서 조회 쿼리 최적화 및 캐싱 전략 변경

--- src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookRepository.kt
+++ src/main/kotlin/com/stepbookstep/server/domain/book/domain/BookRepository.kt
@@ -1,10 +1,13 @@
 package com.stepbookstep.server.domain.book.domain
 
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
-interface BookRepository : JpaRepository<Book, Long> {
+interface BookRepository : JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
     @Query(
         value = "SELECT * FROM books WHERE level = :level ORDER BY RAND() LIMIT 4",
@@ -18,21 +21,12 @@ interface BookRepository : JpaRepository<Book, Long> {
     """)
     fun searchByKeyword(@Param("keyword") keyword: String): List<Book>
 
-    @Query(
-        value = "SELECT * FROM books WHERE genre = :genre ORDER BY RAND() LIMIT 20",
-        nativeQuery = true
-    )
-    fun findRandomByGenre(@Param("genre") genre: String): List<Book>
+    @Query("SELECT b FROM Book b WHERE b.genre = :genre")
+    fun findAllByGenre(@Param("genre") genre: String): List<Book>
 
-    @Query(
-        value = "SELECT * FROM books WHERE item_page < 200 ORDER BY RAND() LIMIT 20",
-        nativeQuery = true
-    )
-    fun findUnder200Pages(): List<Book>
+    @Query("SELECT b FROM Book b WHERE b.itemPage < 200")
+    fun findAllUnder200Pages(): List<Book>
 
-    @Query(
-        value = "SELECT * FROM books WHERE is_bestseller = 1 ORDER BY RAND() LIMIT 20",
-        nativeQuery = true
-    )
-    fun findBestsellers(): List<Book>
+    @Query("SELECT b FROM Book b WHERE b.isBestseller = true")
+    fun findAllBestsellers(): List<Book>
 }
