6eb46e06ce29849ae02d59bdc11fc53d20fa53c9
eun-seoo <kimeunseo040@gmail.com>
2026-01-29 02:28:53 +0900

    chore: 삭제 메서드 트랜잭션 설정 추가

--- src/main/kotlin/com/stepbookstep/server/domain/user/domain/UserCategoryPreferenceRepository.kt
+++ src/main/kotlin/com/stepbookstep/server/domain/user/domain/UserCategoryPreferenceRepository.kt
@@ -1,9 +1,13 @@
 package com.stepbookstep.server.domain.user.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.transaction.annotation.Transactional
 
 interface UserCategoryPreferenceRepository : JpaRepository<UserCategoryPreference, Long> {
 
+    @Modifying
+    @Transactional
     fun deleteAllByUserId(userId: Long)
     fun deleteByUserIdAndCategoryIdIn(userId: Long, categoryIds: Set<Long>)
     fun findAllByUserId(userId: Long): List<UserCategoryPreference>
